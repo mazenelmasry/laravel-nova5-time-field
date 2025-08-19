b4c1f6805c71c0fc573c8b93d664e02e9b2d8dc6
mazen elmasry <mazenalex@gmail.com>
2025-08-19 12:04:01 +0300

    Update Time.php

--- src/Time.php
+++ src/Time.php
@@ -75,4 +75,14 @@ class Time extends Field
             'step' => 0.001,
         ]);
     }
+
+         */
+    public function readonly($callback = true)
+    {
+        if (is_callable($callback)) {
+            $callback = call_user_func($callback, app(\Laravel\Nova\Http\Requests\NovaRequest::class));
+        }
+
+        return $this->withMeta(['readonly' => $callback]);
+    }
 }
